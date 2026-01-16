f626e8cc47c8aa801005134638ff8ce02d653436
carlosmacarro <carlosmaperez@gmail.com>
2026-01-16 16:57:46 +0100

    init

--- recognition/phase.py
+++ recognition/phase.py
@@ -1,3 +1,5 @@
+from nibabel.nifti1 import Nifti1Image
+from totalsegmentator.bin.totalseg_get_phase import get_ct_contrast_phase
 
-def phase_recon():
+def phase_recon(image_obj: Nifti1Image):
     pass
