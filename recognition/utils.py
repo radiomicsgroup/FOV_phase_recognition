12fddae8d60ecdf98a69ad213a3e32c34eda9f4a
carlosmacarro <carlosmaperez@gmail.com>
2026-01-16 16:57:31 +0100

    check uncompatibilites

--- recognition/utils.py
+++ recognition/utils.py
@@ -1,6 +1,16 @@
 import numpy as np
 import nibabel as nib
 
+def check_setting(columns: list[str], no_fov: bool, no_phase: bool):
+    """Check if the settings for FOV and Phase recognition are consistent with existing columns."""
+    if no_fov and 'fov' in columns:
+        raise ValueError("FOV recognition is disabled but 'fov' column exists in the output CSV.")
+    if no_phase and 'phase' in columns:
+        raise ValueError("Phase recognition is disabled but 'phase' column exists in the output CSV.")
+    if not no_fov and 'fov' not in columns:
+        raise ValueError("FOV recognition is enabled but 'fov' column does not exist in the output CSV.")
+    if not no_phase and 'phase' not in columns:
+        raise ValueError("Phase recognition is enabled but 'phase' column does not exist in the output CSV.")
 
 def arr_union(arrs: list[np.ndarray]) -> np.ndarray:
     """Compute the union of multiple binary masks."""
